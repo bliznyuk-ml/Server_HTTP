d28bf7e89a1c47ad09d034e5d1f8127f5be18cf8
Bliznyuk <bliznyuk.ml@gmail.com>
2023-03-27 18:31:18 +0300

    commit v.1

--- src/main/java/org/example/ClientHTTP.java
+++ src/main/java/org/example/ClientHTTP.java
@@ -1,2 +1,63 @@
-package org.example;public class ClientHTTP {
+package org.example;
+
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class ClientHTTP {
+    public static void main(String[] args) {
+        HttpClient httpClient = new HttpClient("localhost", 8080);
+        String response = httpClient.get("/HW_Wiki.html");
+        //System.out.println("-= Response =-");
+        System.out.println(response);
+    }
+}
+
+class HttpClient {
+    private final String host;
+    private final int port;
+
+    public HttpClient(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    private String request(String httpRequest) {
+        StringBuilder responseBuilder = new StringBuilder();
+        // tcp connection
+        try (Socket socket = new Socket()) {
+            socket.connect(new InetSocketAddress(host, port));
+            if (socket.isConnected()) {
+                // http protocol
+                try (OutputStream out = socket.getOutputStream();
+                     InputStream in = socket.getInputStream();
+                     BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
+                    out.write(httpRequest.getBytes()); // send request to server
+                    String line;
+                    while ((line = reader.readLine()) != null) { // get response
+                        responseBuilder.append(line).append("\n");
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return responseBuilder.toString();
+    }
+
+    String get(String path) {
+        String request = getRequest(path);
+        //System.out.println("-= Request =-");
+        //System.out.println(request);
+        return request(request);
+    }
+
+    private String getRequest(String path) {
+        return "GET %s HTTP/1.1\r\n".formatted(path) +
+                "Host: %s\r\n".formatted(host) +
+                "User-Agent: Java Http Client\r\n" +
+                "Accepted: text/http\r\n" +
+                "Keep-Alive: close\r\n" +
+                "\r\n";
+    }
 }
--- src/main/java/org/example/ServerHTTP.java
+++ src/main/java/org/example/ServerHTTP.java
@@ -1,13 +1,75 @@
 package org.example;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Date;
+
 /**
- * Hello world!
- *
+ * 1. Створити простий http однопоточний сервер для статичних веб сторінок
+ * <p>
+ * * Сервер повинен слухати порт 8080
+ * * Статичні веб сторінки повинні знаходитися в папці html проекту.
+ * * Якщо сторінка, що запитує клієнт, знаходиться в html, повернути її з кодом 200
+ * * У випадку якщо потрібного файлу в html не буде, повернути відповідь з кодом 404 і сторінкою 404.html (повинна бути в html)
+ * * Перевірити, що браузер дозволяє відтворити сторінки, що повертає сервер
+ * * Сервер повинен продовжувати роботу після відповіді на перший запит і повертати сторінки при наступних запитах
+ * <p>
+ * 2. Додаткове завдання
+ * *  Створити багатопоточний веб сервер
  */
-public class ServerHTTP
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class ServerHTTP {
+
+    public static final int PORT = 8080;
+
+    Date date = new Date();
+
+
+    public static void main(String[] args) throws IOException {
+        String response200 = "HTTP/1.1 200 OK\r\n" +
+                "\r\n";
+        String response404 = "HTTP/1.1 404 Not Found\r\n" +
+                "\r\n";
+        ServerSocket server = new ServerSocket(PORT);
+        StringBuilder sb = new StringBuilder();
+        System.out.println("Wait for connection...");
+        Socket remoteClient = server.accept();
+        System.out.println("Client connected at " + remoteClient.getRemoteSocketAddress().toString());
+        try (InputStream inResponse = remoteClient.getInputStream();
+             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inResponse));
+             OutputStream out = remoteClient.getOutputStream();
+             PrintStream printStream = new PrintStream(out, true)) {
+
+            String line;
+            int fileNameIndex = 2;
+            line = bufferedReader.readLine();
+            String[] index = line.split("\\/|\\s");
+            //System.out.println(index[fileNameIndex]);
+            String fileName = "html\\" + index[fileNameIndex];
+            //System.out.println(fileName);
+            Path path = Paths.get(fileName);
+            boolean exists = Files.exists(path);
+            if (exists) {
+                try (InputStream inFile = new FileInputStream(fileName)) {
+                    while (inFile.available() > 0) {
+                        byte[] bytes = inFile.readNBytes(1024);
+                        out.write(response200.getBytes());
+                        out.write(bytes);
+                    }
+                }
+            } else {
+                try (InputStream inFile = new FileInputStream("html\\404.html")) {
+                    while (inFile.available() > 0) {
+                        byte[] bytes = inFile.readNBytes(1024);
+                        out.write(response404.getBytes());
+                        out.write(bytes);
+                    }
+                }
+            }
+        }
     }
 }
